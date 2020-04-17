2300b4852f1f59e561199935ccae0245427ce0a3
Hermann KOUADIO <kouadiohermann6@gmail.com>
2020-04-17 23:57:57 +0000

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { SidemenuComponent } from './components/layout/sidemenu/sidemenu.component';
+import { SigninComponent } from './components/site/signin/signin.component';
+import { SignupComponent } from './components/site/signup/signup.component';
+import { DashboardComponent } from './components/site/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidemenuComponent,
+    SigninComponent,
+    SignupComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
