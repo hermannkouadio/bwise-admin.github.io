0469e1ade99209f8c8b434af4ffb3dc724061c2a
Hermann KOUADIO <kouadiohermann6@gmail.com>
2020-06-09 12:48:48 +0000

    geroku deploy

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/SBADMINBS4'));
+app.use(express.static(__dirname + '/dist/BSADMINBS4'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/SBADMINBS4/index.html'));
+res.sendFile(path.join(__dirname+'/dist/BSADMINBS4/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8099, ()=>{
+    console.log('Server started on port 8099');
+})
